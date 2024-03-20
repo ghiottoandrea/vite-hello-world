0c4e73504a1cac5ae3adbe3100df05f41a7c7b9f
ghiottoandrea <andreaghiotto9@gmail.com>
2024-03-20 15:45:53 +0100

    Add Hello World

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script>
 export default {
   // my option object
+  data() {
+    return {
+      message: `Hello World`
+    }
+  }
 }
 </script>
 
 <template>
+  <h1>{{ message }}</h1>
 </template>
 
 <style></style>
